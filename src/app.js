2691d54875ab4d6ef4a1a90b218cda9a5b2e741d
Tiago Lourinho <lourinho.tiago@gmail.com>
2025-01-29 22:11:27 -0300

    Add `views` folder configuration

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 import express from "express";
 
-import { PORT } from "./config/constants.js";
+import { PORT, VIEWS_DIR } from "./config/constants.js";
 import { serverStart } from "./utils/tools.js";
 
 const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", VIEWS_DIR);
 
 app.listen(PORT, (err) => serverStart(err, PORT));
--- src/config/constants.js
+++ src/config/constants.js
@@ -1,1 +1,8 @@
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const configFile = fileURLToPath(import.meta.url);
+
 export const PORT = 3000;
+export const ROOT_DIR = dirname(configFile).replace("/config", "");
+export const VIEWS_DIR = path.join(ROOT_DIR, "views");
