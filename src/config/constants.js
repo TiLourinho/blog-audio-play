e60fa6a5ab2e7a0e892ffc8270a3cdfec7f62e9b
Tiago Lourinho <lourinho.tiago@gmail.com>
2025-01-29 23:07:30 -0300

    Create `main.css` file and add static files access

--- src/config/constants.js
+++ src/config/constants.js
@@ -6,3 +6,4 @@ const configFile = fileURLToPath(import.meta.url);
 export const PORT = 3000;
 export const ROOT_DIR = dirname(configFile).replace("/config", "");
 export const VIEWS_DIR = path.join(ROOT_DIR, "views");
+export const PUBLIC_DIR = path.join(ROOT_DIR, "public");
