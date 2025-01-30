307ed582a3290254d850854cc915bdd51f2941a0
Tiago Lourinho <lourinho.tiago@gmail.com>
2025-01-29 22:44:00 -0300

    Add `create.ejs` file

--- src/app.js
+++ src/app.js
@@ -8,4 +8,8 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", VIEWS_DIR);
 
+app.get("/create", (_req, res) => {
+  res.render("create");
+});
+
 app.listen(PORT, (err) => serverStart(err, PORT));
